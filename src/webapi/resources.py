e60ca5f005b304b42216cca3fcfb14f12773dc87
Ogro <ogro@levva.io>
2024-05-27 15:50:54 -0300

    Changed API dev to work with Postman

--- src/webapi/resources.py
+++ src/webapi/resources.py
@@ -4,7 +4,7 @@ from flask_restx import Resource, Namespace
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model.models import Player
-from json import dumps
+import json
 from resource_fields import player_post
 
 player_ns = Namespace("player")
@@ -17,35 +17,39 @@ class player_resources(Resource):
     def get(self):
         session = Session()
         players = session.query(Player).all()
+        import pdb; pdb.set_trace()
         players_dicts = [player.to_dict() for player in players]
-        serialized_players = dumps(players_dicts)
-        return serialized_players
+        return players_dicts
 
-    @api.expect(player_post)
+    @player_ns.expect(player_post)
     def post(self):
-        data = request.data
+
+        data = request.form
 
         new_player = Player(
-            name=data['name'],
-            nick=data['nick'],
-            twitch=data['twitch'],
-            availability=data['availability'],
-            coins=data['coins'],
-            stars=data['stars'],
-            medal=data['medal'],
-            wins=data['wins'],
-            tags=data['tags'],
-            email=data['email'],
-            photo=data['photo']
+            name=data.get('name'),
+            nick=data.get('nick'),
+            twitch=data.get('twitch'),
+            availability=data.get('availability'),
+            coins=data.get('coins'),
+            stars=data.get('stars'),
+            medal=data.get('medal'),
+            wins=data.get('wins'),
+            tags=data.get('tags'),
+            email=data.get('email'),
+            photo=data.get('photo')
         )
 
         session = Session()
-        session.add(new_player)
-
-        session.commit()
-
-        session.close()
-
-        return jsonify(new_player.to_dict()), 201
+        try:
+            session.add(new_player)
+            session.commit()
+            session.refresh(new_player)  # Refresh na instancia para evitar erros
+            return new_player.to_dict()
+        except Exception as e:
+            session.rollback()
+            return jsonify({'error': str(e)}), 500
+        finally:
+            session.close()
 
                 
