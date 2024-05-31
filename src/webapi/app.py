d4001ac3a7aff14bc6112f53e12d11a7ef29f719
JogueOgro <131224720+JogueOgro@users.noreply.github.com>
2024-05-31 13:19:15 -0300

    Adding Draft to DB WIP

--- src/webapi/app.py
+++ src/webapi/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from resources import player_ns, coin_ns
+from flask_cors import CORS
+from resources import draft_ns, player_ns, coin_ns
 from api import api
 
 app = Flask(__name__)
 api.init_app(app)
+api.add_namespace(draft_ns)
 api.add_namespace(player_ns)
 api.add_namespace(coin_ns)
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/webapi/resources.py
+++ src/webapi/resources.py
@@ -3,14 +3,15 @@ from flask import request, jsonify
 from flask_restx import Resource, Namespace
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model.models import Player
+from model.models import *
 import json
 from resource_fields import player_post
 
+draft_ns = Namespace("draft")
 player_ns = Namespace("player")
 coin_ns = Namespace("coin")
 
-engine = create_engine("mysql://root:root@localhost:3306/mydb")
+engine = create_engine("mysql://root:root@localhost:3306/rivvals")
 Session = sessionmaker(bind=engine)
 
 @player_ns.route("/")
@@ -52,9 +53,67 @@ class player_resources(Resource):
         finally:
             session.close()
 
+@draft_ns.route("/")
+class draft_resources(Resource):
+    def get(self):
+        return "OK"
+
+    def post(self):
+        session = Session()
+        try:
+          players = json.loads(request.data.decode('unicode_escape')).get('players')
+
+          new_players = []
+          for player in players:
+              new_player = session.query(Player).filter_by(name=player['name']).first()
+              if new_player:
+                  # Se o jogador já existir, atualize seus atributos
+                  new_player.nick = player.get('nick')
+                  new_player.twitch = player.get('twitch')
+                  new_player.availability = player.get('availability')
+                  new_player.coins = player.get('coins')
+                  new_player.stars = player.get('stars')
+                  new_player.medal = player.get('medal')
+                  new_player.wins = player.get('wins')
+                  new_player.tags = player.get('tags')
+                  new_player.email = player.get('email')
+                  new_player.photo = player.get('photo')
+                  new_players.append(new_player)
+              else:
+                  # Se o jogador não existir, crie um novo jogador
+                  new_player = Player(
+                      name=player.get('name'),
+                      nick=player.get('nick'),
+                      twitch=player.get('twitch'),
+                      availability=player.get('availability'),
+                      coins=0,
+                      stars=player.get('stars'),
+                      medal=player.get('medal'),
+                      wins=player.get('wins'),
+                      tags=player.get('tags'),
+                      email=player.get('email'),
+                      photo=player.get('photo')
+                  )
+                  new_players.append(new_player)
+
+
+          session.add_all(new_players)
+          session.commit()
+          for player in new_players:
+              session.refresh(player)  # Refresh na instancia para evitar erros
+          return [player.to_dict() for player in new_players]
+
+        except Exception as e:
+          session.rollback()
+          return str(e)#jsonify({'error': str(e)}), 500
+        finally:
+          session.close()
+
 @coin_ns.route("/")
 class coin_resources(Resource):
     def get(self):
-        return "coins"
+        session = Session()
+        players = session.query(CoinsLog).all()
+        players_dicts = [player.to_dict() for player in players]
+        return players_dicts
 
-                
