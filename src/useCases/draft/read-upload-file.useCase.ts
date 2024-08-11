e73f0cbbc1566fee3558cb6a06557a9f61a62166
JogueOgro <131224720+JogueOgro@users.noreply.github.com>
2024-08-11 15:24:03 -0300

    Fixing type error

--- src/useCases/draft/read-upload-file.useCase.ts
+++ src/useCases/draft/read-upload-file.useCase.ts
@@ -15,8 +15,8 @@ type IParams = {
   callBack: () => void
 }
 
-const execute = async ({ draft, callBack, type }: IParams) => {
-  if (!draft) return
+const execute = async ({ file, callBack, type }: IParams) => {
+  if (!file) return
 
   const { config } = draftStore.getState()
 
